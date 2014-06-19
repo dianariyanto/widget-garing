e0f30e7f931dd8cfef5299b43fcbff4aa34f0472
Ngrambe <aryan@gravis-design.com>
2014-06-20 01:04:38 +0700

    Versi Awal
    Masih terlalu dini untuk disebutkan

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Widget Garing
+Plugin URI: http://ngrambe.net/wordpress/widget-garing/
+Description: Plugin membuat widget wordpress modifikasi.
+Author: Aryan
+Author URI: http://ngrambe.net/
+Text Domain: widget-garing
+Domain Path: /
+Version: 0.1
+*/
+
+?>
